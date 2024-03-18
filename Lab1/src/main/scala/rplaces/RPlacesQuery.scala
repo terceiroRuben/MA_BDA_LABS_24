e666eedf09042be57c03cb4c37cb285773ac7394
Raphael Besson <r.besson@outlook.com>
2024-03-18 20:04:57 +0100

    Plus que le point 6 et tester autres fichiers

--- Lab1/src/main/scala/rplaces/RPlacesQuery.scala
+++ Lab1/src/main/scala/rplaces/RPlacesQuery.scala
@@ -39,7 +39,7 @@ object RPlacesQuery {
     def main(args: Array[String]): Unit = {
         val paintsRDD = loadPaintsRDD()
         val q = new RPlacesQuery(spark, sc)
-        val canvas = Canvas(500, 500)
+        val canvas = Canvas(500, 500) // De base, c'était (2000,2000). J'ai changé car le fichier utilisé pour le moment est 500x500
 
         val beforeWhitening = timed("Last colored event", q.lastColoredEvent(paintsRDD))
         println(beforeWhitening)
@@ -108,6 +108,7 @@ class RPlacesQuery(spark: SparkSession, sc: SparkContext) {
         lastNonWhiteEvent.timestamp
     }
 
+    // Output : 2022-04-04T22:47:37.197Z
     
     //
     // 4. Compute a ranking of colors
@@ -118,36 +119,66 @@ class RPlacesQuery(spark: SparkSession, sc: SparkContext) {
     }
 
     def rankColors(colors: List[ColorIndex], rdd: RDD[PaintEvent]): List[(ColorIndex, Long)] = {
-        colors.map(color => (color, occurencesOfColor(color, rdd)))
-            .sortWith(_._2 > _._2)
+        colors.map(color => (color, occurencesOfColor(color, rdd))).sortWith(_._2 > _._2)
     }
 
+    // Processing naive ranking took 15242 ms.
+    // List((27,1616699), (31,1461618), (2,706722), (12,578426), (4,431632), (13,253347), (3,237898), (18,225761), (14,218502), (8,180505), (6,146267), (1,140549), (29,139909), (23,132582), (25,131534),
+    //(30,119143), (19,76291), (24,68159), (26,58186), (7,53937), (28,53001), (22,49074), (15,40300), (9,32348), (5,24705), (17,23087), (0,20461), (21,19174), (16,17895), (20,11626), (10,8137), (11,6533))
+
     def makeColorIndex(rdd: RDD[PaintEvent]): RDD[(ColorIndex, Iterable[PaintEvent])] = {
-        ???
+        rdd.map(event => (event.pixel_color_index, event)).groupByKey()
     }
 
     def rankColorsUsingIndex(index: RDD[(ColorIndex, Iterable[PaintEvent])]): List[(ColorIndex, Long)] = {
-        ???
+        index.mapValues(_.size.toLong).collect().toList.sortWith(_._2 > _._2)
     }
+
+    // Processing ranking using inverted index took 6538 ms.
+    // List((27,1616699), (31,1461618), (2,706722), (12,578426), (4,431632), (13,253347), (3,237898), (18,225761), (14,218502), (8,180505), (6,146267), (1,140549), (29,139909), (23,132582), (25,131534),
+    //(30,119143), (19,76291), (24,68159), (26,58186), (7,53937), (28,53001), (22,49074), (15,40300), (9,32348), (5,24705), (17,23087), (0,20461), (21,19174), (16,17895), (20,11626), (10,8137), (11,6533))
+
+    //Première tentative (classement naïf) : 15242 ms (environ 15,2 secondes)
+    //Deuxième tentative (utilisant un index inversé) : 6538 ms (environ 6,5 secondes)
+
+    //Le temps d'exécution est meilleur due à l'efficacité de l'indexation dans la deuxième méthode. La liste de classements des couleurs reste cependant la  même pour les deux méthodes.
+    //En utilisant un index, on pointe directement vers où chaque couleur est utilisée, ce qui évite de chercher dans tout le dataset pour chaque couleur. 
+    //Cela simplifie le travail en organisant les données une seule fois et permet à Spark de traiter les données plus rapidement.
 
     def rankColorsReduceByKey(rdd: RDD[PaintEvent]): List[(ColorIndex, Long)] = {
-        ???
+        rdd.map(event => (event.pixel_color_index, 1L)).reduceByKey(_ + _).collect().toList.sortWith(_._2 > _._2)
     }
 
+    //Processing ranking using reduceByKey took 1005 ms.
+    //List((27,1616699), (31,1461618), (2,706722), (12,578426), (4,431632), (13,253347), (3,237898), (18,225761), (14,218502), (8,180505), (6,146267), (1,140549), (29,139909), (23,132582), (25,131534), 
+    //(30,119143), (19,76291), (24,68159), (26,58186), (7,53937), (28,53001), (22,49074), (15,40300), (9,32348), (5,24705), (17,23087), (0,20461), (21,19174), (16,17895), (20,11626), (10,8137), (11,6533))
+
+    //Il y a une nette amélioration de performance avec reduceByKey qui a mis seulement 1005 ms, contre plus pour les autres méthodes. En gros, reduceByKey travaille directement sur les données
+    //pour les combiner de manière intelligente et rapide, sans passer par l'étape de création d'un index comme avant. C'est plus direct et donc plus rapide.
 
     //
     // 5. Find for the n users who have placed the most pixels, their busiest tile
     //
 
     def nMostActiveUsers(rdd: RDD[PaintEvent], n: Int = 10): List[(UserId, Long)] = {
-        ???
+        rdd.map(event => (event.id, 1L)).reduceByKey(_ + _).takeOrdered(n)(Ordering[Long].reverse.on(x => x._2)).toList
     }
+
+    //List((85899866930,559), (17180084138,443), (68719685381,432), (42950200594,429), (25770217927,422), (34360264713,420), (103079423385,415), (68719954231,412), (51540074706,406), (17180078961,403))
 
     def usersBusiestTile(rdd: RDD[PaintEvent], activeUsers: List[UserId], canvas: Canvas): List[(UserId, (TileId, Long))] = {
         // You can use canvas.tileAt(x, y) to know the tile of a coordinate.
-        ???
+        rdd.filter(event => activeUsers.contains(event.id))
+            .map(event => ((event.id, canvas.tileAt(event.coordinate_x, event.coordinate_y)), 1L))
+            .reduceByKey(_ + _)
+            .map { case ((userId, tileId), count) => (userId, (tileId, count)) }
+            .groupByKey()
+            .mapValues(iter => iter.maxBy(_._2))
+            .collect()
+            .toList
     }
 
+    //List((42950200594,(185,255)), (25770217927,(167,136)), (17180084138,(169,230)), (68719685381,(169,205)), (34360264713,(177,174)), (51540074706,(176,104)), (85899866930,(197,495)), (103079423385,(199,126)), (17180078961,(195,79)), (68719954231,(167,282)))
 
     //
     // 6. Find the percentage of painting event at each coordinate which have the same color as just before the whitening
